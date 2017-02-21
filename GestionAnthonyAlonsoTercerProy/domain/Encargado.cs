71180c2668d9cbfdb1ea22317dbed5c8978984e9
Anthony <tony950819@gmail.com>
2017-02-20 21:36:13 -0600

    Proyecto

---
+++ GestionAnthonyAlonsoTercerProy/domain/Encargado.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain
+{
+    public class Encargado
+    {
+        private String cedula;
+        private String nombre;
+        private String apellido;
+        private String apellidoDos;
+        private int telefono;
+        private String correo;
+        private String direccion;
+
+        public string Cedula
+        {
+            get
+            {
+                return cedula;
+            }
+
+            set
+            {
+                cedula = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return apellido;
+            }
+
+            set
+            {
+                apellido = value;
+            }
+        }
+
+        public string ApellidoDos
+        {
+            get
+            {
+                return apellidoDos;
+            }
+
+            set
+            {
+                apellidoDos = value;
+            }
+        }
+    }
+}
